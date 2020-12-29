a1397bbb017801b51e86389447e70aba87e19da6
David Soards <davidysoards@gmail.com>
2020-12-29 18:03:07 -0500

    revert to postcss 7

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    // require('tailwindcss'),
     require('postcss-nested'),
     require('autoprefixer'),
-    // process.env.NODE_ENV === 'production' ? require('cssnano') : false,
   ],
 };
