50fb5158276716657899fffd39131e05154d90ae
David Soards <davidysoards@gmail.com>
2020-12-28 21:58:08 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    // 'no-console': 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-unsafe-finally': 'warn',
+    // 'no-unused-vars': 'warn'
+  },
+};
